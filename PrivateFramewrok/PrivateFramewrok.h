7e73d0e01adecf82616cf95f907cd0789fc1f343
Ari Munandar <arimunandar.dev@gmail.com>
2018-12-05 11:16:13 +0700

    Add files via upload (#2)

---
+++ PrivateFramewrok/PrivateFramewrok.h
@@ -0,0 +1,19 @@
+//
+//  PrivateFramewrok.h
+//  PrivateFramewrok
+//
+//  Created by Ari Munandar on 05/12/18.
+//  Copyright © 2018 Netzme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrivateFramewrok.
+FOUNDATION_EXPORT double PrivateFramewrokVersionNumber;
+
+//! Project version string for PrivateFramewrok.
+FOUNDATION_EXPORT const unsigned char PrivateFramewrokVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrivateFramewrok/PublicHeader.h>
+
+
